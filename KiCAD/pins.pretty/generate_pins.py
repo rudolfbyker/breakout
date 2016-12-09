5a888023985bf45b0e605f6c6fa90d3abb5e6109
Rudolf Byker <rudolfbyker@gmail.com>
2016-12-09 14:58:47 +0200

    Update names, descriptions and silkscreen value

--- KiCAD/pins.pretty/generate_pins.py
+++ KiCAD/pins.pretty/generate_pins.py
@@ -11,21 +11,17 @@ dir_my_pins = "./"
 re_filenames = re.compile(r"Pin_Header_Straight_(\d)x(\d\d).kicad_mod")
 
 
-modules = []
 for filename in os.walk(dir_kicad_pins).next()[2]:
     matches = re_filenames.match(filename)
     if matches:
-        modules.append({
-            'input_filename': filename,
-            'module': KicadMod(dir_kicad_pins + filename),
-            'shape': matches.groups()
-        })
 
+        module = KicadMod(dir_kicad_pins + filename)
+        shape = matches.groups()
 
-for module in modules:
+        for pad in module.pads:
+            pad['drill']['size'] = {'x': 1., 'y': 1.}
 
-    for pad in module['module'].pads:
-        pad['drill']['size'] = {'x': 1., 'y': 1.}
-
-    module['module'].name = "pin-{}x{}".format(module['shape'][0], module['shape'][1])
-    module['module'].save(filename="{}{}.kicad_mod".format(dir_my_pins, module['module'].name))
+        module.name = "pin-{}x{}".format(shape[0], shape[1])
+        module.description = "Header pin. 2.54mm pitch. 1mm drills. Shape {}x{}".format(shape[0], shape[1])
+        module.value['value'] = module.name
+        module.save(filename="{}{}.kicad_mod".format(dir_my_pins, module.name))
